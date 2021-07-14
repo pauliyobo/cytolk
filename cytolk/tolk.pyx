f5a051df08ccd32cdfe30129ee3b7a6c77deee95
pauliyobo <pauliyobo@gmail.com>
2021-07-14 23:10:02 +0200

    An exception will now be raised if the function that is being called require s the library to be loaded first

--- cytolk/tolk.pyx
+++ cytolk/tolk.pyx
@@ -1,6 +1,7 @@
 # cython: language_level=3
 # distutils: language="c++"
 
+from functools import wraps
 import os
 from pathlib import Path
 from typing import Optional
@@ -11,24 +12,48 @@ from . cimport tolk
 
 dir: str = str(Path(__file__).parent) + os.pathsep
 
+
+class TolkNotLoadedError(BaseException):
+    pass
+
+
+def check_if_loaded(fn):
+    """
+    This decorator takes care of calling the function supplied
+    raising a TolkNotLoadedError if tolk was not loaded
+    """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if not is_loaded():
+            raise TolkNotLoadedError(f"tolk must be loaded to call {fn.__name__}")
+        return fn(*args, **kwargs)
+    return wrapper
+
+
 def add_dll_path():
     """this function adds the dll directory in order to allow tolk to properly find the dlls it needs without having to place them manually."""   
     os.environ['PATH'] = dir + os.environ['Path']
+
 
 def remove_dll_path():
     # forgive me for the really really not elegant way
     os.environ['PATH'] = os.environ['PATH'].replace(dir, "")
 
+
 def load():
-    add_dll_path()
-    tolk.Tolk_Load()
+    if not is_loaded():
+        add_dll_path()
+        tolk.Tolk_Load()
+
 
 def unload():
-    tolk.Tolk_Unload()
-    remove_dll_path()
+    if is_loaded():
+        tolk.Tolk_Unload()
+        remove_dll_path()
 
 def is_loaded() -> bool:
     return tolk.Tolk_IsLoaded()
+
 
 def detect_screen_reader()-> Optional[str]:
     cdef const wchar_t* sr = tolk.Tolk_DetectScreenReader()
@@ -36,29 +61,38 @@ def detect_screen_reader()-> Optional[str]:
         return None
     return sr
 
+
 def try_sapi(try_sapi: bool):
     tolk.Tolk_TrySAPI(try_sapi)
+
 
 def prefer_sapi(prefer_sapi: bool):
     tolk.Tolk_PreferSAPI(prefer_sapi)
 
+@check_if_loaded
 def output(text: str, interrupt: bool=False) -> bool:
     return tolk.Tolk_Output(text, interrupt)
+
 
 def speak(text: str, interrupt: bool=False) -> bool:
     return tolk.Tolk_Speak(text, interrupt)
 
+
 def braille(text: str):
     return tolk.Tolk_Braille(text)
+
 
 def has_speech() -> bool:
     return tolk.Tolk_HasSpeech()
 
+
 def has_braille() -> bool:
     return tolk.Tolk_HasBraille()
 
+
 def is_speaking() -> bool:
     return tolk.Tolk_IsSpeaking()
+
 
 def silence() -> bool:
     return tolk.Tolk_Silence()
