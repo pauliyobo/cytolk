8b9d4b47994fa007a14d11467f543b4a1f213550
pauliyobo <pauliyobo@gmail.com>
2023-10-04 10:26:12 +0200

    Provided support for python 3.12

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ for lib in libs.glob("*.dll"):
 setup(
     name = "cytolk",
     cmdclass  = {"build_ext": build_ext},
-    version = "0.1.12",
+    version = "0.1.13",
     description = "A cython wrapper over the tolk library",
     long_description_content_type='text/markdown', 
     long_description = get_readme(),
